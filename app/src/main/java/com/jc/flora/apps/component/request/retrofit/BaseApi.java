82ded3b1ab1ec20005d97aa8a3930710e1744e11
JcFlora <shijincheng64@163.com>
2018-02-28 21:25:50 +0800

    retrofit支持低版本https请求

--- app/src/main/java/com/jc/flora/apps/component/request/retrofit/BaseApi.java
+++ app/src/main/java/com/jc/flora/apps/component/request/retrofit/BaseApi.java
@@ -2,6 +2,12 @@ package com.jc.flora.apps.component.request.retrofit;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.jc.flora.apps.component.deviceinfo.DeviceUtil;
+
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.X509TrustManager;
 
 import io.reactivex.Observer;
 import io.reactivex.annotations.NonNull;
@@ -24,16 +30,18 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
  */
 public class BaseApi {
 
-    private static final String HOST = "http://gank.io/api/";
+    private static final String HOST = "https://gank.io/api/";
 
-    protected Retrofit getRetrofit(){
+    protected Retrofit getRetrofit() {
         return sRetrofit;
     }
 
     private static Retrofit sRetrofit = initRetrofit();
 
     private static Retrofit initRetrofit() {
-        OkHttpClient httpClient = new OkHttpClient.Builder().addInterceptor(new GzipRequestInterceptor()).build();
+        OkHttpClient.Builder builder = new OkHttpClient.Builder().addInterceptor(new GzipRequestInterceptor());
+        initSSLSocketFactory(builder);
+        OkHttpClient httpClient = builder.build();
         //使用GsonBuilder创建Gson，统一日期请求格式
         Gson gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
@@ -47,15 +55,44 @@ public class BaseApi {
                 .build();
     }
 
+    private static void initSSLSocketFactory(OkHttpClient.Builder builder) {
+        if (DeviceUtil.isSystemVersionAfterKitkatWatch()) {
+            return;
+        }
+        try {
+            // 自定义一个信任所有证书的TrustManager，添加SSLSocketFactory的时候要用到
+            X509TrustManager trustAllCert = new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return new java.security.cert.X509Certificate[]{};
+                }
+            };
+            SSLSocketFactory sslSocketFactory = new SSLSocketFactoryCompat(trustAllCert);
+            builder.sslSocketFactory(sslSocketFactory, trustAllCert);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static abstract class ObserverAdapter<T> implements Observer<T> {
         @Override
         public void onSubscribe(@NonNull Disposable d) {
         }
+
         @Override
-        public void onComplete(){
+        public void onComplete() {
         }
+
         @Override
-        public void onError(Throwable e){
+        public void onError(Throwable e) {
         }
     }
 
