837dac4700d590844c1e80dc0d435fe6640a10cc
Shijincheng <shijincheng64@163.com>
2018-05-21 15:09:30 +0800

    约束布局实现弧形布局

--- app/src/main/java/com/jc/flora/apps/ui/arclayout/ArcLayoutActivity.kt
+++ app/src/main/java/com/jc/flora/apps/ui/arclayout/ArcLayoutActivity.kt
@@ -1,7 +1,7 @@
 package com.jc.flora.apps.ui.arclayout
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.jc.flora.R
@@ -9,11 +9,12 @@ import com.jc.flora.apps.Project
 import com.jc.flora.apps.ProjectsAdapter
 import com.jc.flora.apps.ui.arclayout.projects.ArcLayout1Activity
 import com.jc.flora.apps.ui.arclayout.projects.ArcLayout6Activity
+import com.jc.flora.apps.ui.arclayout.projects.ArcLayout7Activity
 import com.jc.flora.launcher.NotFoundActivity
-import java.util.ArrayList
+import java.util.*
 
 /**
- * compile 'com.ogaclejapan.arclayout:library:1.1.0@aar'
+ * implementation 'com.ogaclejapan.arclayout:library:1.1.0@aar'
  * https://github.com/ogaclejapan/ArcLayout
  * Created by shijincheng on 2017/2/8.
  */
@@ -29,32 +30,44 @@ class ArcLayoutActivity : AppCompatActivity() {
     private fun addProjects() {
         val rvProjects = findViewById(R.id.rv_projects) as RecyclerView
         rvProjects.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        rvProjects.adapter = ProjectsAdapter(this, getProjects())
+        rvProjects.adapter = ProjectsAdapter(this, projects)
     }
 
-    private fun getProjects() : ArrayList<Project>{
-            return arrayListOf(getProject1(), getProject2(), getProject6())
-    }
+    private val projects: ArrayList<Project>
+        get() {
+            return arrayListOf(project1, project2, project6, project7)
+        }
 
-    private fun getProject1(): Project {
-        val project = Project()
-        project.projectName = "基础弧形布局"
-        project.targetActivity = ArcLayout1Activity::class.java
-        return project
-    }
+    private val project1: Project
+        get() {
+            val project = Project()
+            project.projectName = "基础弧形布局"
+            project.targetActivity = ArcLayout1Activity::class.java
+            return project
+        }
 
-    private fun getProject2(): Project {
-        val project = Project()
-        project.projectName = "Path菜单"
-        project.targetActivity = NotFoundActivity::class.java
-        return project
-    }
+    private val project2: Project
+        get() {
+            val project = Project()
+            project.projectName = "Path菜单"
+            project.targetActivity = NotFoundActivity::class.java
+            return project
+        }
 
-    private fun getProject6(): Project {
-        val project = Project()
-        project.projectName = "弧形Tab+指示器"
-        project.targetActivity = ArcLayout6Activity::class.java
-        return project
-    }
+    private val project6: Project
+        get() {
+            val project = Project()
+            project.projectName = "弧形Tab+指示器"
+            project.targetActivity = ArcLayout6Activity::class.java
+            return project
+        }
+
+    private val project7: Project
+        get() {
+            val project = Project()
+            project.projectName = "使用约束布局实现弧形布局"
+            project.targetActivity = ArcLayout7Activity::class.java
+            return project
+        }
 
 }
---
+++ app/src/main/java/com/jc/flora/apps/ui/arclayout/projects/ArcLayout7Activity.java
@@ -0,0 +1,20 @@
+package com.jc.flora.apps.ui.arclayout.projects;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jc.flora.R;
+
+/**
+ * Created by shijincheng on 2018/5/21.
+ */
+public class ArcLayout7Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("使用约束布局实现弧形布局");
+        setContentView(R.layout.activity_arclayout7);
+    }
+
+}
